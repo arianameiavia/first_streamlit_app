c8267b476469d8b3d166bccb8ca44312b76e81c6
arianameiavia <81562241+arianameiavia@users.noreply.github.com>
2023-03-01 16:05:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     # output in the screen as table
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
   
